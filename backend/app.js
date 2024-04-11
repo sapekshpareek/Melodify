525750055da6e59e668751388ba1ee59df4b37ef
Sapeksh Pareek <sapekshpareek136@gmail.com>
2024-04-11 21:27:30 +0530

    Database: Model Creation

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(8080, () => {
-    console.log("Server in running on http://localhost:8080/")
-})
+  console.log("Server in running on http://localhost:8080/");
+});
---
+++ backend/models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const User = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  likedSongs: {
+    type: String,
+    default: "",
+  },
+  likedPlaylist: {
+    type: String,
+    default: "",
+  },
+  subscribedArtits: {
+    type: String,
+    default: "",
+  },
+});
+
+const UserModel = mongoose.model("User", User);
+
+module.exports = UserModel;
