7ea31e1532db8a5b6a05cceb62a5f39b227cd761
sapekshpareek <sapekshpareek136@gmail.com>
2024-08-08 17:47:38 +0530

    Login Page Start

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,20 @@
 import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginComponent from "./routes/Login";
 // import './App.css'
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className ="w-screen h-screen">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<div>hi</div>}></Route>
           <Route path="/login" element={ <LoginComponent/> }></Route>
         </Routes>
       </BrowserRouter>
-    </>
+    </div>
   );
 }
 
---
+++ frontend/src/routes/Login.jsx
@@ -0,0 +1,15 @@
+import { Icon } from "@iconify/react";
+
+const LoginComponent = () => {
+    return <div className="w-full h-full flex flex-col items-center">
+        <div className="logo p-5 border-b border-solid border-grey-200 w-full flex justify-center">
+
+        <Icon icon="logos:spotify" width={150}/>
+        </div>
+        <div className="inputRegion">
+
+        </div>
+    </div>;
+};
+
+export default LoginComponent;
