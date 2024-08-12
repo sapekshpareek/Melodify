18d702aebeb1ae27e44bbc7c1bdfba50906bd5e5
sapekshpareek <sapekshpareek136@gmail.com>
2024-08-12 13:38:10 +0530

    Navbar Completion

--- frontend/src/components/shared/RoundedButton.jsx
+++ frontend/src/components/shared/RoundedButton.jsx
@@ -1,11 +1,19 @@
+import { Icon } from "@iconify/react";
 import { Link } from "react-router-dom";
 
-const RoundedButton = ({placeholder, className, path}) => {
-    return <button className={`border border-solid border-gray-500 rounded-full w-full py-3 my-2 text-base text-gray-500 font-semibold ${className}`}>
-        <Link to={path}>
-        {placeholder}
-        </Link>
-    </button>;
+const RoundedButton = ({ placeholder, className, path, iconName }) => {
+  return (
+    <button
+      className={`border border-solid border-gray-500 rounded-full w-full py-3 my-2 text-base text-gray-500 font-semibold ${className}`}
+    >
+      <div className="flex items-center justify-center">
+        <div className="px-2">
+          <Icon icon={iconName} />
+        </div>
+        <Link to={path}>{placeholder}</Link>
+      </div>
+    </button>
+  );
 };
 
 export default RoundedButton;
