5d963591c06cbb5cf07eee0426832654b969dd29
sapekshpareek <sapekshpareek136@gmail.com>
2024-08-13 14:13:51 +0530

    CSS Changes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        "1/10": "10%",
+        "9/10": "90%",
+      },
+      backgroundColor: {
+        "app-black": "#121212",
+      },
+    },
   },
   plugins: [],
-}
-
+};
